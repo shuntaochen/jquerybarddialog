f238541436dd02a2a62d6c87d65e04029519016d
Calos <359000081@qq.com>
2016-09-13 11:28:12 +0800

    change to a more slim version

--- jquery.tableheadersticky.js
+++ jquery.tableheadersticky.js
@@ -1,34 +1,8 @@
-﻿/// <reference path="jquery-2.1.1.min.js" />
-/**
+﻿/**
  * @license jquery.tableheaderstick 1.0.0
  * (c) 2010-infinity China, Shanghai. CalosChen
  * License: MIT
  * Usage: $(table).stick();
- * Description: A jquery plugin which creates a stuck header for raw html table even when scrolling.
+ * Description: A jquery plugin which creates a stuck header for origin html table even when scrolling.
  */
-
-; (function ($) {
-    $.fn.stick = function () {
-        var $t = $(this);
-        var h = $t.attr('s_sticked');
-        if (h) return;
-        var $w = function () { return $t.width(); };
-        var $scope = $t.closest('div').css({ 'position': 'relative', 'overflow': 'hidden' });
-        $('<style type="text/css">.f { background-color:#ffffff; position: absolute; top: 0; left: 0; }</style>').appendTo('head');
-        $t.wrap('<div class="b" style="width:100%;height:100%;overflow:auto;" />').before($t.clone().removeAttr('id').addClass('f'));
-        var $f = $('.f', $scope);
-        $('tbody', $f).empty();
-        function resize() {
-            $f.width($w());
-            $('th', $f).each(function (i) {
-                $(this).width($('th', $t).eq(i).width());
-            });
-        }
-        $('.b', $scope).scroll(function () {
-            $f.css('left', 0 - $(this).scrollLeft())
-        })
-        $(window).resize(resize);
-        resize();
-        $t.attr('s_sticked', true);
-    }
-}(jQuery));
+(function (n) { n.fn.stick = function () { function f() { i.width(u()); n("th", i).each(function (i) { n(this).width(n("th", t).eq(i).width()) }) } var t = n(this), e = t.attr("s_sticked"), u, r, i; e || (u = function () { return t.width() }, r = t.closest("div").css({ position: "relative", overflow: "hidden" }), n('<style type="text/css">.f { background-color:#ffffff;z-index:1; position: absolute; top: 0; left: 0; }<\/style>').appendTo("head"), t.wrap('<div class="b" style="width:100%;height:100%;overflow:auto;" />').before(t.clone().removeAttr("id").addClass("f")), i = n(".f", r), n("tbody", i).empty(), n(".b", r).scroll(function () { i.css("left", 0 - n(this).scrollLeft()) }), n(window).resize(f), f(), t.attr("s_sticked", !0)) } })(jQuery);
